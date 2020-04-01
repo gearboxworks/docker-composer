1be6549f3695c5ac9a0fdf251cf223b56b24163d
MickMake <github@mickmake.com>
2020-04-01 11:26:47 +1100

    docker-template v1.2

--- 1.3.3/build/unit-tests/composer/02-release.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Created on 2020-03-17T16:34:00+1100, using template:02-release.sh.tmpl and json:gearbox.json
-
-p_info "composer-1.3.3" "Release test started."
-
-ps -eaf
-
-p_info "composer-1.3.3" "Release test finished."
---
+++ versions/1.3.3/build/unit-tests/composer/02-release.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Created on 2020-03-17T16:34:00+1100, using template:02-release.sh.tmpl and json:gearbox.json
+
+p_info "composer-1.3.3" "Release test started."
+
+ps -eaf
+
+p_info "composer-1.3.3" "Release test finished."
